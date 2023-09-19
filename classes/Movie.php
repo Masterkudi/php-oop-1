4b087a3ba3bc15124613d783cd9e55f146b063ed
masterkudi <cudini.andrea@gmail.com>
2023-09-19 17:31:00 +0200

    Impostazione esercizio

---
+++ classes/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class Movie {
+    public $titolo;
+    public $anno;
+    public $regista;
+    public $paese;
+    public $feat;
+    public DateTime $durata;
+
+    
+}
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+    require_once __DIR__ . '/classes/Movie.php';
+
+    $movie1 = new Movie();
+
+    var_dump($movie1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FontAwesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Bootstrap links -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
+    <!-- Axios -->
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <!-- CSS link -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <!-- Vue.js -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <!-- Title -->
+
+    <title>PHP-OOP-1</title>
+
+</head>
+
+<body>
+
+</body>
+
+</html>
