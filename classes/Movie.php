292059ee65ff7a360e366a634fa7095d2f878036
masterkudi <cudini.andrea@gmail.com>
2023-09-20 18:55:39 +0200

    aggiornamento

--- classes/Movie.php
+++ classes/Movie.php
@@ -8,12 +8,17 @@ class Movie {
     public $country;
     public $poster;
 
-    //function __construct($title, $year, $imdbID, $country, $poster) {}
+    /* function __construct($title, $year, $imdbID, $country, $poster) {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->imdbID = $_imdbID;
+        $this->country = $_country;
+        $this->poster = $_poster;
+    } */
 
     public function getFullMovie() {
         return $this->title . '' . $this->year . '' . $this->imdbID . '' . $this->country . '' . $this->poster;
     }
 };
-
 
 ?>
--- index.php
+++ index.php
@@ -3,49 +3,47 @@ require_once __DIR__ . '/classes/Movie.php';
 
 $movie1 = new Movie();
 
-    $movie1->title = 'The Lord of the Rings: The Fellowship of the Ring';
-    $movie1->year = '2001';
-    $movie1->imdbID = 'tt0120737';
-    $movie1->country = 'NZ/USA';
-    $movie1->poster = 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg';
+$movie1->title = 'The Lord of the Rings: The Fellowship of the Ring';
+$movie1->year = '2001';
+$movie1->imdbID = 'tt0120737';
+$movie1->country = 'NZ/USA';
+$movie1->poster = 'https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg';
 
-    echo $movie1->getFullMovie();
+echo $movie1->getFullMovie();
 
 
 $movie2 = new Movie();
 
-    $movie2->title = 'The Lord of the Rings: The Two Towers';
-    $movie2->year = '2002';
-    $movie2->imdbID = 'tt0167261';
-    $movie2->country = 'NZ/USA';
-    $movie2->poster = 'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg';
+$movie2->title = 'The Lord of the Rings: The Two Towers';
+$movie2->year = '2002';
+$movie2->imdbID = 'tt0167261';
+$movie2->country = 'NZ/USA';
+$movie2->poster = 'https://m.media-amazon.com/images/M/MV5BNGE5MzIyNTAtNWFlMC00NDA2LWJiMjItMjc4Yjg1OWM5NzhhXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg';
 
-    echo $movie2->getFullMovie();
+echo $movie2->getFullMovie();
 
 $movie3 = new Movie();
 
-    $movie3->title = 'The Lord of the Rings: The Return of the King';
-    $movie3->year = '2003';
-    $movie3->imdbID = 'tt0167260';
-    $movie3->country = 'NZ/USA';
-    $movie3->poster = 'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg';
+$movie3->title = 'The Lord of the Rings: The Return of the King';
+$movie3->year = '2003';
+$movie3->imdbID = 'tt0167260';
+$movie3->country = 'NZ/USA';
+$movie3->poster = 'https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg';
 
-    echo $movie3->getFullMovie();
+echo $movie3->getFullMovie();
 
 
 $movie4 = new Movie();
 
-    $movie4->title = 'Lord of War';
-    $movie4->year = '2005';
-    $movie4->imdbID = 'tt0399295';
-    $movie4->country = 'movie';
-    $movie4->poster = 'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg';
+$movie4->title = 'Lord of War';
+$movie4->year = '2005';
+$movie4->imdbID = 'tt0399295';
+$movie4->country = 'USA';
+$movie4->poster = 'https://m.media-amazon.com/images/M/MV5BMTYzZWE3MDAtZjZkMi00MzhlLTlhZDUtNmI2Zjg3OWVlZWI0XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX300.jpg';
 
-    echo $movie4->getFullMovie();
+echo $movie4->getFullMovie();
 
-    var_dump($movie1, $movie2, $movie3, $movie4);
-
-    
+var_dump($movie1, $movie2, $movie3, $movie4);
 
 ?>
 
